9cf2313442ccd5daf7dc74b81ebb57fb65b24a0c
hexiandong <1170833484@qq.com>
2021-07-08 20:45:43 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home1</router-link>
+    |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+
+}
+
+#nav {
+
+}
+</style>
